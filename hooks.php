f753e2ba92caac59d50f98ecbdf9aa2357a9ec84
Kevin Fraser <kevin@ksfraser.com>
2026-05-16 15:31:01 -0600

    Simplify hooks, update config to 2.4.3, move code to common utilities

--- hooks.php
+++ hooks.php
@@ -7,47 +7,9 @@
 
 define('SS_PM', 131 << 8);
 
-class hooks_fa_projectmanagement extends hooks {
+class hooks_ksf_FA_ProjectManagement extends hooks {
 
-    private function ensure_composer_dependencies(): void {
-        $module_dir = dirname(__FILE__);
-        $autoload_path = $module_dir . '/vendor/autoload.php';
-        
-        if (!file_exists($autoload_path)) {
-            $composer_path = $module_dir . '/composer.json';
-            if (file_exists($composer_path)) {
-                chdir($module_dir);
-                $output = [];
-                $return_code = 0;
-                exec('composer install --no-interaction --prefer-dist 2>&1', $output, $return_code);
-                if ($return_code !== 0) {
-                    error_log('KSF Module: composer install failed: ' . implode("\n", $output));
-                }
-            }
-        }
-    }
-    var $module_name = 'fa_projectmanagement';
-
-    private function ensure_composer_dependencies(): void {
-        $module_dir = dirname(__FILE__);
-        $autoload_path = $module_dir . '/vendor/autoload.php';
-        
-        if (!file_exists($autoload_path)) {
-            $composer_path = $module_dir . '/composer.json';
-            if (file_exists($composer_path)) {
-                chdir($module_dir);
-                $output = [];
-                $return_code = 0;
-                exec('composer install --no-interaction --prefer-dist 2>&1', $output, $return_code);
-                if ($return_code !== 0) {
-                    error_log('KSF Module: composer install failed: ' . implode("\n", $output));
-                }
-            }
-        }
-    }
-    var $version = '1.0.0';
-
-    private function ensure_composer_dependencies(): void {
+    private function ensure_composer_dependencies() {
         $module_dir = dirname(__FILE__);
         $autoload_path = $module_dir . '/vendor/autoload.php';
         
@@ -96,6 +58,13 @@ class hooks_fa_projectmanagement extends hooks {
         $security_areas['SA_PMCREATE'] = array(SS_PM | 2, _("Create Projects"));
         $security_areas['SA_PMMANAGE'] = array(SS_PM | 3, _("Manage Projects"));
         return array($security_areas, $security_sections);
+    }
+
+    function install_extension($check_only=true) {
+        return true;
+    }
+
+    function install_tabs($app) {
     }
 
     function activate_extension($company, $check_only=true) {
